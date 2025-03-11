e7da66d0b9fe5aa52cfb2cace10d4196a8112bdc
Robin Roosmaa <Robin.Roosmaa@vikk.ee>
2025-03-11 20:22:26 +0200

    Commenteeritud

--- Ülessanne 1.py
+++ Ülessanne 1.py
@@ -5,9 +5,11 @@ pygame.display.set_caption("Lumemees - Robin Roosmaa") # Määrame pygame pealki
 white = (255, 255, 255) # Määrame vajatud värvide väärtused.
 black = (0, 0, 0)
 red = (255, 0, 0)
+# Keha
 pygame.draw.circle(screen, white, (150, 75), 30)  # Lumememme ülemine pall
 pygame.draw.circle(screen, white, (150, 140), 40) # Lumememme keskmine pall
 pygame.draw.circle(screen, white, (150, 225), 50) # Lumememme alumine pall
+# Pea
 pygame.draw.circle(screen, black, (140, 70), 5)  # Vasak silm
 pygame.draw.circle(screen, black, (160, 70,), 5)  # Parem silm
 pygame.draw.polygon(screen, red, ((145, 80), (155, 80), (150, 95))) # Nina
@@ -17,7 +19,6 @@ while jooksmas:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jooksmas = False
-
     pygame.display.flip()
 
 pygame.quit()

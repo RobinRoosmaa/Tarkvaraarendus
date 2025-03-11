0c336f44762798d0696e46cd021322263691d9f6
Robin Roosmaa <Robin.Roosmaa@vikk.ee>
2025-03-11 19:44:04 +0200

    Commenteeritud

--- Ülessanne 1.py
+++ Ülessanne 1.py
@@ -1,25 +1,22 @@
-import pygame
-pygame.init()
-screen=pygame.display.set_mode([300,300])
-pygame.display.set_caption("Lumemees - Robin Roosmaa")
-white = (255, 255, 255)
+import pygame # Impordime pygame raamatukogu
+pygame.init() # Alustame pygame funktsioonid.
+screen=pygame.display.set_mode([300,300]) # Määrame pygame akna laiuseks 300 x 300 pixlit.
+pygame.display.set_caption("Lumemees - Robin Roosmaa") # Määrame pygame pealkirjaks "Lumemees - Robin Roosmaa".
+white = (255, 255, 255) # Määrame vajatud värvide väärtused.
 black = (0, 0, 0)
 red = (255, 0, 0)
-pygame.draw.circle(screen, white, (150, 75), 30)  # Head
-pygame.draw.circle(screen, white, (150, 140), 40) # Midsection
-pygame.draw.circle(screen, white, (150, 225), 50) # Base
-
-# Draw facial features
-pygame.draw.circle(screen, black, (140, 70), 5)  # Left eye
-pygame.draw.circle(screen, black, (160, 70,), 5)  # Right eye
-pygame.draw.polygon(screen, red, ((145, 80), (155, 80), (150, 95)))
-
-# Game loop (to keep the window open)
-running = True
-while running:
+pygame.draw.circle(screen, white, (150, 75), 30)  # Lumememme ülemine pall
+pygame.draw.circle(screen, white, (150, 140), 40) # Lumememme keskmine pall
+pygame.draw.circle(screen, white, (150, 225), 50) # Lumememme alumine pall
+pygame.draw.circle(screen, black, (140, 70), 5)  # Vasak silm
+pygame.draw.circle(screen, black, (160, 70,), 5)  # Parem silm
+pygame.draw.polygon(screen, red, ((145, 80), (155, 80), (150, 95))) # Nina
+#While tsükkel akna lahti hoidmiseks.
+jooksmas = True
+while jooksmas:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            jooksmas = False
 
     pygame.display.flip()
 
